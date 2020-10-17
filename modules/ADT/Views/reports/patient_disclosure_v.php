ad99929409221bad473ee207a50f35c6ddc315a9
KevinMarete <kevomarete@gmail.com>
2020-10-17 14:58:47 +0300

    feat: updated report views to CI4 standard

--- modules/ADT/Views/reports/patient_disclosure_v.php
+++ modules/ADT/Views/reports/patient_disclosure_v.php
@@ -1,18 +1,16 @@
-<div id="wrapperd">			
+<div id="wrapperd">
 	<div id="patient_enrolled_content" class="full-content">
-		<?php $this->load->view("reports/reports_top_menus_v") ?>
-		<h4 style="text-align: center" id='report_title'>Patient Status &amp; Disclosure Between <span  id="start_date"><?php echo $from;?></span> and <span  id="start_date"><?php echo $to;?></span></h4>
+		<?php echo view("\Modules\ADT\Views\\reports\\reports_top_menus_v"); ?>
+		<h4 style="text-align: center" id='report_title'>Patient Status &amp; Disclosure Between <span id="start_date"><?php echo $from; ?></span> and <span id="start_date"><?php echo $to; ?></span></h4>
 		<hr size="1" style="width:80%">
-         <div id='disclosure_chart'></div>
+		<div id='disclosure_chart'></div>
 	</div>
 </div>
-<script src="<?=base_url() ?>assets/scripts/highcharts/highcharts.js" ></script>
+<script src="<?= base_url() ?>public/assets/scripts/highcharts/highcharts.js"></script>
 
 <script type="text/javascript">
-	$(document).ready(function(){	
-		$("#disclosure_chart").load("<?php echo base_url().'report_management/disclosure_chart/'.$from.'/'.$to ?>")
-	
+	$(document).ready(function() {
+		$("#disclosure_chart").load("<?php echo base_url() . 'public/report_management/disclosure_chart/' . $from . '/' . $to ?>")
+
 	});
 </script>
-
-	
--- modules/ADT/Views/reports/patients_nonadherence_v.php
+++ modules/ADT/Views/reports/patients_nonadherence_v.php
@@ -1,15 +1,13 @@
-
-<div  class="full-content">
-	<?php $this->load->view("reports/reports_top_menus_v")
-	?>
-	<h4 style="text-align: center"><?php echo $report_title;?>
-	<br>
-	<br>
-	Between
-	<input type="text" id="start_date" value="<?php echo $from; ?>">
-	and
-	<input type="text" id="end_date" value="<?php echo $to; ?>">
+<div class="full-content">
+	<?php echo view("\Modules\ADT\Views\\reports\\reports_top_menus_v"); ?>
+	<h4 style="text-align: center"><?php echo $report_title; ?>
+		<br>
+		<br>
+		Between
+		<input type="text" id="start_date" value="<?php echo $from; ?>">
+		and
+		<input type="text" id="end_date" value="<?php echo $to; ?>">
 	</h4>
 	<hr size="1" style="width:80%">
-	<?php echo $dyn_table;?>
+	<?php echo $dyn_table; ?>
 </div>
