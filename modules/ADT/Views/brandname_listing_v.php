c6c495c149405a7bfab8dc1153d97c91dea0b499
Alphonce <alpho07@gmail.com>
2020-11-03 18:48:30 +0300

    Drug Script Source

--- modules/ADT/Views/brandname_listing_v.php
+++ modules/ADT/Views/brandname_listing_v.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="<?php echo base_url() . '/public/assets/Scripts/datatable/jquery.dataTables.rowGrouping.js' ?>"></script>
+<script type="text/javascript" src="<?php echo base_url() . '/public/assets/scripts/datatable/jquery.dataTables.rowGrouping.js' ?>"></script>
 
 
 <script>
