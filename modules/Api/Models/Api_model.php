4473e35ba94c74a2c085d2d14f69892a0465bd91
Kenneth Lebu <kenlebu@gmail.com>
2020-11-10 10:52:50 +0300

    Fix query error

--- modules/Api/Models/Api_model.php
+++ modules/Api/Models/Api_model.php
@@ -67,19 +67,19 @@ class Api_model extends BaseModel {
         //$CI = &get_instance();
         //$CI->load->database();
         $cond = '';
-        $query_str = "SELECT p.*,ps.name as patient_status,pso.name as patient_source ,g.name as patient_gender FROM patient p
-		left join patient_status ps on p.current_status = ps.id 
-		left join patient_source pso on p.source = pso.id
-		left join gender g on g.id = p.gender
-		WHERE p.patient_number_ccc   = '$internal_id' ";
+        $query_str = "SELECT p.*,ps.name as patient_status,pso.name as patient_source ,g.name as patient_gender FROM patient p ".
+		"left join patient_status ps on p.current_status = ps.id ".
+		"left join patient_source pso on p.source = pso.id ".
+		"left join gender g on g.id = p.gender ".
+		"WHERE p.patient_number_ccc = '".$internal_id."' ";
 
         // do left join in the case of patient created on adt and not already on IL
 
 
-        $query = $this->db->query($query_str);
+        $query = DB::select($query_str);
 
-        if (count($query->getResult()) > 0) {
-            $returnable = $query->getResult()[0];
+        if (count($query) > 0) {
+            $returnable = $query[0];
         } else {
             $returnable = false;
         }
