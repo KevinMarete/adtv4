c582e30dd9f65c7c624ca29fb50e1dbe7c2f2f06
Kenneth Lebu <kenlebu@gmail.com>
2020-11-10 09:46:01 +0300

    Fix api auth filters

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -38,6 +38,7 @@ class Filters extends BaseConfig {
                             'migrate/*',
                             'github',
                             'help',
+                            'api',
                             'api/*'
                         ]
                     ]
